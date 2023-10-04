4ebfdf548a74400f7279e4018063fb02190a5abb
neukym <acgt2@cam.ac.uk>
2023-10-04 13:54:25 +0100

    Update scripts

--- audio-delay-accuracy/matlab-presentation/matlab_latency_test.m
+++ audio-delay-accuracy/matlab-presentation/matlab_latency_test.m
@@ -6,44 +6,31 @@ reqlatencyclass=4;
 sugglatency=[]; % not changing anything this.
 InitializePsychSound(1);
 devices = PsychPortAudio('GetDevices');
-deviceid = 6; %windows direct sound              184, 24, 22, 23, 22, 23, 22 (!) % Does use accept two sound handels.
+deviceid = 6; % use Windows WASAPI
 MySoundHandle = PsychPortAudio('Open', deviceid, [], reqlatencyclass, freq, channels, 48, sugglatency);
-
-%Delay asio  first pulse 15ms, and then everyone from the point is 83 ms from that trigger
-
-%with direct sound the first pulse is 127 ms and then 83 thereafter
-
-%MME all over the place and jittery.
-
-%osccioscpoel trigering from the NI card and then measuing the soundcard output and comparing the distance betert the two for the delay.
-
-%And its consistant.
 
 PsychPortAudio('FillBuffer', MySoundHandle, wavedata');
 
     
-    % Perform one warmup trial, to get the sound hardware fully up and running,
-    % performing whatever lazy initialization only happens at real first use.
-    % This "useless" warmup will allow for lower latency for start of playback
-    % during actual use of the audio driver in the real trials:
-    %PsychPortAudio('Start', MySoundHandleA, 1, 0, 1);
-    %PsychPortAudio('Stop', MySoundHandleA, 1);
+% Perform one warmup trial, to get the sound hardware fully up and running,
+% performing whatever lazy initialization only happens at real first use.
+% This "useless" warmup will allow for lower latency for start of playback
+% during actual use of the audio driver in the real trials
+     
+PsychPortAudio('Start', MySoundHandle, 1, 0, 0, 0.001);
+PsychPortAudio('Stop', MySoundHandle, 1);
     
-
-
 
 MEG = MEGSynchClass;
 MEG.SendTrigger(0);
 WaitSecs(0.5);
-
-PsychPortAudio('Start', MySoundHandle, 1, inf, 0);
 
 for i=1:10
     WaitSecs(0.5);
     tic
 
     MEG.SendTrigger(1);
-    PsychPortAudio('RescheduleStart', MySoundHandle, 1, 0, 1);
+    PsychPortAudio('Start', MySoundHandle, 1, 0, 0);
     
     toc
     MEG.SendTrigger(0);
--- audio-drift-accuracy/matlab-presentation/matlab_drift_test.m
+++ audio-drift-accuracy/matlab-presentation/matlab_drift_test.m
@@ -15,14 +15,13 @@ deviceid = 6;
 MySoundHandle = PsychPortAudio('Open', deviceid, [], reqlatencyclass, freq, channels, buffersize, sugglatency);
 
 
-
 PsychPortAudio('FillBuffer', MySoundHandle, wavedata');
 
     
-    % Perform one warmup trial, to get the sound hardware fully up and running,
-    % performing whatever lazy initialization only happens at real first use.
-    % This "useless" warmup will allow for lower latency for start of playback
-    % during actual use of the audio driver in the real trials:
+% Perform one warmup trial, to get the sound hardware fully up and running,
+% performing whatever lazy initialization only happens at real first use.
+% This "useless" warmup will allow for lower latency for start of playback
+% during actual use of the audio driver in the real trials:
 
 PsychPortAudio('Start', MySoundHandle, 1, 0, 0, 0.001);
 PsychPortAudio('Stop', MySoundHandle, 1);
